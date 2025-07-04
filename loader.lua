7d1d6cd931035a0c996dbac31efec722233b3e4b
Coemprosad <shiayeiny@gmail.com>
2025-07-04 16:37:16 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Shiayein/ImmortalFarm/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shiayein/ImmortalFarm/main/verification.lua"))()
