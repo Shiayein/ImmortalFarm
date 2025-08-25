cf29ac833bec7618d72afb0c96f968baa86f5a2a
Coemprosad <shiayeiny@gmail.com>
2025-08-25 17:33:12 +0200

    Add files via upload

---
+++ key_system.lua
@@ -0,0 +1,14 @@
+------------------------------------------------------------------
+-- key_system.lua
+--  Ajouter / gérer les clés ici.
+--    nil  = clé encore libre
+--    number = UserId du joueur à qui la clé est liée
+------------------------------------------------------------------
+
+local Keys = {
+    ["IMMORTAL2025"] = 7849930474,   -- déjà réservée
+    ["SECRETKEY001"] = nil,          -- libres :
+    ["VIPACCESS99"]  = nil,
+}
+
+return Keys
